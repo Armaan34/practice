3823c8e404fd92ae34ea8f2861fca4997b9ddf02
Armaan <harmaan34@gmail.com>
2023-08-02 17:43:24 +0000

    w

---
+++ 3 copy 2.js
@@ -0,0 +1,12 @@
+let i = 50
+
+for(let i = 0; i<= 4; i++){
+    console.log(i);
+}
+
+console.log(i);
+// let i = 10;
+i = 10
+console.log(i);
+
+// let can only be declared once, not twice or more in the same scope
