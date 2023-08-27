938064c8956f126ea55cf44a4fe9ad2e682bd495
Armaan <harmaan34@gmail.com>
2023-08-27 17:50:56 +0000

    e

---
+++ rw/4 copy 2.js
@@ -0,0 +1,15 @@
+const a = 2;
+
+a = 4
+
+
+
+console.log(a);
+
+// re-assignment to const is not allowed
+
+/*
+    var - you can declare and re-declare
+    let - you cannot re-declare it again in the same block
+    const - you cannot re-declare it again in the same block or re-assign its value
+*/
