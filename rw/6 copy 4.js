13b4dde43c60896471fe906349ae307d5382bdb4
Armaan <harmaan34@gmail.com>
2023-08-29 17:58:14 +0000

    n

---
+++ rw/6 copy 4.js
@@ -0,0 +1,12 @@
+// OOP
+// what is an object
+// what is a class
+// why oop
+
+/*
+    JavaScript is a heavily Object Oriented Programming Language
+    It does oop : 
+        1) Prototype Based ( functions )
+        2) Class
+
+*/
