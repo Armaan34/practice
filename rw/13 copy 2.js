c3c97f302ac4406ca338ba8603f7b0b928c84285
Armaan <harmaan34@gmail.com>
2023-09-01 18:12:12 +0000

    n

---
+++ rw/13 copy 2.js
@@ -0,0 +1,9 @@
+/* 
+
+Write a function called randomCard that generates a random card from a standard deck of playing cards (52 cards). 
+A card is represented as a string in the format "Rank of Suit" (e.g. "Ace of Spades").
+
+*/
+
+var ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+var suits = ['Clubs', "Diamonds", "Hearts", "Spades"]
