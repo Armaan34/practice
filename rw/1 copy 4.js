5fa54790e6c3722a053ad728d8cfca8f60ea9c5a
Armaan <harmaan34@gmail.com>
2023-09-03 18:19:40 +0000

    new

---
+++ rw/1 copy 4.js
@@ -0,0 +1,32 @@
+function hello(){
+    console.log("hello")
+} // function declaration
+
+// function invocation
+// hello() 
+
+function greeting(name){// parameter
+    console.log("good morning", name);
+}
+
+// greeting("jayesh")
+// greeting("samreen")
+
+// let n1 = 10;
+// let n2 = 20;
+// let sum = n1 + n2;
+// console.log(sum);
+
+// n1 = 30;
+// n2 = 5;
+// sum = n1 + n2;
+// console.log(sum);
+
+function sum(a,b){
+    console.log(a + b);
+}
+
+sum(10,20)
+sum(12,5)
+sum(13,90)
+
