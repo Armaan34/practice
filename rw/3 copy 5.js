f34cbd30b8d72c4901736caf5b362b2a550dc3ac
Armaan <harmaan34@gmail.com>
2023-09-03 18:18:56 +0000

    e

---
+++ rw/3 copy 5.js
@@ -0,0 +1,19 @@
+function addNumbers( num1 , num2) {
+    console.log(num1 + num2);
+}
+
+addNumbers(10,3)
+
+var addTwo = (num1 , num2) =>{
+    console.log(num1 + num2);
+}
+
+addTwo(20,30)
+
+var greeting = name =>{
+    console.log("hello ,",name);
+}
+greeting("ateeq")
+
+var greeting2 = name => console.log("hi ,",name)
+greeting2("ateeq")
