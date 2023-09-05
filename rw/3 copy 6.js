f66daac05a79a794b57277c3996a47e7897beb1a
Armaan <harmaan34@gmail.com>
2023-09-05 18:19:24 +0000

    e

---
+++ rw/3 copy 6.js
@@ -0,0 +1,24 @@
+// For Loop
+
+// for (var i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for(var i = -1000 ; i <= -1000; i--){
+//     console.log(i);
+// }
+
+var counter = 0
+
+for(var i = 0 ; i < 1000 ; i+=3){
+    console.log(i);
+    counter++;
+}
+
+
+// for(var i = 0 ; i < 1000 ; i++){
+//     if(i%3==0) console.log(i);
+//     counter++;
+// }
+
+console.log("counter : ", counter);
