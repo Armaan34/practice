0355631692db24990adc348faa62f795e133bf9c
Armaan <harmaan34@gmail.com>
2023-09-05 18:20:03 +0000

    new

---
+++ rw/1 copy 5.js
@@ -0,0 +1,30 @@
+var a = 1;
+
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+
+//while loop
+
+// while (a <= 15) {
+//   console.log(a);
+//   a++;
+// }
+
+while (a <= 100) {
+    console.log(a);
+    a++;
+  }
+  
