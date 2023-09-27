7618efeb24aaa7e9258d6b431c64caea96cbdf31
Armaan <harmaan34@gmail.com>
2023-09-27 17:30:57 +0000

    e

---
+++ rw/sendMail.js
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer"
+import config from "config"
+
+
+
+async function sendEmail(emailBody) {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: config.get("EMAIL_SMTP.HOST"),
+            port: 465,
+            // secure: true,
+            auth: {
+                user: config.get("EMAIL_SMTP.AUTH.USER"),
+                pass: config.get("EMAIL_SMTP.AUTH.PASSWORD"),
+            },
+        });
+
+        const info = await transporter.sendMail({
+            from: '"Team Haider" <kb0107@haider101.online>', // sender address
+            to: emailBody.to, // list of receivers
+            subject: emailBody.subject, // Subject line
+            text: emailBody.body,
+        });
+
+        console.log("Message sent: %s", info.messageId);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default sendEmail
