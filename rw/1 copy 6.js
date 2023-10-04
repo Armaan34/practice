ac097b86dc953c06376555fe3f190415e885fa14
Armaan <harmaan34@gmail.com>
2023-10-04 17:23:22 +0000

    n

---
+++ rw/1 copy 6.js
@@ -0,0 +1,35 @@
+/*
+value =>  1 or 2 or 3
+
+switch(value){
+    case 1:
+        console.log("case 1");
+        break;
+    case 2:
+        console.log("case 2");
+        break;
+    case 3:
+        console.log("case 3");
+        break;
+    default:
+        console.log("default")
+        break;
+}
+*/
+
+var value = "hello";
+
+switch(value){
+    case "hi":
+        console.log("case 1");
+        break
+    case "hello":
+        console.log("case 2");
+        break
+    case "hola":
+        console.log("case 3");
+        break
+    default:
+        console.log("default")
+        break
+}
