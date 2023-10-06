d6f2a1c46783555a765b40562a1353ac5d9b95bf
Armaan <harmaan34@gmail.com>
2023-10-06 17:41:20 +0000

    n

---
+++ rw/1 copy 7.js
@@ -0,0 +1,15 @@
+var obj = {
+    a : 2,
+    b : "hello",
+    c : {
+        aa: 4
+    }
+}
+
+// console.log(obj);
+
+// dot operator
+
+console.log(obj.b);
+console.log(obj.a);
+console.log(obj.c.aa);
